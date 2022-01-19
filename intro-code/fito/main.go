cc7474e61c142977f21400513b5a8f79b87baf9e
fito <fito.prolog@gmail.com>
2022-01-18 16:28:56 -0800

    some cleanup

--- intro-code/fito/main.go
+++ intro-code/fito/main.go
@@ -11,8 +11,6 @@ func main() {
   if len(os.Args) < 2 {
     panic(fmt.Sprintf("use %s [config_name]",os.Args[0])) 
   }
-  //c,_:=config.Parse(fmt.Sprintf("config_json/%s_config.json",os.Args[1]))
-  //fmt.Printf(c.ConnectionString())
   err:=services.Serve(fmt.Sprintf("config_json/%s_config.json",os.Args[1]))
   if err != nil{
     panic(err.Error())
